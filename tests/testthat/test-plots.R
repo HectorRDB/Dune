9bc1244658e2f456f645fd4649624c5b3a70297b
Hector <hector.rouxdebezieux@berkeley.edu>
2020-03-03 11:35:32 -0800

    Create test-plots.R

---
+++ tests/testthat/test-plots.R
@@ -0,0 +1,22 @@
+library(Dune)
+library(dplyr)
+
+test_that("All static plotting functions return a ggplot object", {
+  data("clusMat", package = "Dune")
+  data("nuclei", package = "Dune")
+  merger <- Dune(clusMat)
+  expect_is(plotARIs(merger$initialMat), "gg")
+  expect_is(plotARIs(merger$initialMat,
+                     unclustered = 1), "gg")
+  expect_is(plotPrePost(merger), "gg")
+  expect_is(ARItrend(merger), "gg")
+  expect_is(ConfusionPlot(nuclei[, c("SC3", "Monocle")]), "gg")
+})
+
+test_that("All dynamic plotting functions return a gganim object", {
+  data("clusMat", package = "Dune")
+  data("nuclei", package = "Dune")
+  merger <- Dune(clusMat)
+  expect_is(ARIEvolution(merger), "gganim")
+  expect_is(ConfusionEvolution(merger, x = "A", y = "B"), "gganim")
+})
